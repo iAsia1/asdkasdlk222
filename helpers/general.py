cddb7f69d458f4aea63d7104c257af3fcb380920
chuck <stefan.stefanov28@lethsd.ab.ca>
2025-01-30 13:28:30 -0700

    Add files via upload

---
+++ helpers/general.py
@@ -0,0 +1,18 @@
+class Generator:
+    def __init__(self, gen):
+        self.gen = gen
+
+    def __iter__(self):
+        self.value = yield from self.gen
+
+def get_free_sa(sa_map,file_size):
+    # ok_sas = []
+    tmp = []
+    for i in sa_map:
+        if 15784004812 - int(i['size']) >=file_size:
+            tmp.append([int(i['size']),int(i['_id'])])
+            # ok_sas.append(int(i['_id']))
+    tmp.sort(key=lambda x:x[0])
+    ok_sas = [i[1] for i in tmp]
+    return ok_sas
+
